e960227ef26f96fdf59ed6c6e17cee2c821c94ab
eis <eis@iki.fi>
2017-11-11 13:22:44 +0200

    postgresql version needs to be specified to avoid https://askubuntu.com/questions/50621/cannot-connect-to-postgresql-on-port-5432

--- ubuntu-deps.sh
+++ ubuntu-deps.sh
@@ -1,3 +1,3 @@
 apt-get update
-apt-get install -y postgresql postgresql-contrib openjdk-8-jdk maven
+apt-get install -y postgresql-9.5 postgresql-contrib openjdk-8-jdk maven
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
