51d3873646018ab190587918d6039b32105642d1
eis <eis@iki.fi>
2017-12-02 17:03:36 +0200

    remove unused imports

--- src/main/java/fi/eis/applications/highscore/HighScoreApplication.java
+++ src/main/java/fi/eis/applications/highscore/HighScoreApplication.java
@@ -1,9 +1,7 @@
 package fi.eis.applications.highscore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
