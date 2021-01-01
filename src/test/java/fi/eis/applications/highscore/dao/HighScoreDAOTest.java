76a131bdf9c429401c0e3d008865cc29c1af8c91
eis <eis@iki.fi>
2021-01-01 22:47:29 +0200

    Spring 2.4.1 update

--- src/test/java/fi/eis/applications/highscore/dao/HighScoreDAOTest.java
+++ src/test/java/fi/eis/applications/highscore/dao/HighScoreDAOTest.java
@@ -1,20 +1,20 @@
 package fi.eis.applications.highscore.dao;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import javax.validation.ConstraintViolationException;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import fi.eis.applications.highscore.test.HighScoreEntryVOFactory;
+import fi.eis.applications.highscore.vo.HighScoreEntryVO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import fi.eis.applications.highscore.test.HighScoreEntryVOFactory;
-import fi.eis.applications.highscore.vo.HighScoreEntryVO;
+import javax.validation.ConstraintViolationException;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class HighScoreDAOTest {
     @Autowired
@@ -23,11 +23,14 @@ public class HighScoreDAOTest {
     @Autowired
     private HighScoreDAO highScoreDao;
 
-    @Test(expected = ConstraintViolationException.class)
-    public void confirmConstraintChecking() throws Exception {
-        entityManager.persist(HighScoreEntryVOFactory.testEntryNoDate());
-        highScoreDao.findAll().iterator().next();
+    @Test
+    public void confirmConstraintChecking() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            entityManager.persist(HighScoreEntryVOFactory.testEntryNoDate());
+            highScoreDao.findAll().iterator().next();
+        });
     }
+
     @Test
     public void confirmReturnEntry() throws Exception {
         entityManager.persist(HighScoreEntryVOFactory.testEntry());
--- src/test/java/fi/eis/applications/highscore/vo/HighScoreEntryVOJsonTest.java
+++ src/test/java/fi/eis/applications/highscore/vo/HighScoreEntryVOJsonTest.java
@@ -1,18 +1,17 @@
 package fi.eis.applications.highscore.vo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import fi.eis.applications.highscore.json.DateISO8601Serializer;
+import fi.eis.applications.highscore.test.HighScoreEntryVOFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import fi.eis.applications.highscore.json.DateISO8601Serializer;
-import fi.eis.applications.highscore.test.HighScoreEntryVOFactory;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @JsonTest
 public class HighScoreEntryVOJsonTest {
 
